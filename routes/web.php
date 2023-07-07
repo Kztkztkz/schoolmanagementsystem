bc98759046d372564b2b70158848117872c9aedb
Kztkztkz <131935171+Kztkztkz@users.noreply.github.com>
2023-07-07 11:45:29 +0630

    implement template
    add template / define modules / naming conventions

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',function(){
+    return view('scheduler.scheduler');
 });
+
+Route::resource('/class','App\Http\Controllers\ClassController');
+
+Route::resource('/course','App\Http\Controllers\CourseController');
+
+Route::resource('/student','App\Http\Controllers\StudentController');
+
+Route::resource('/payment','App\Http\Controllers\PaymentController');
+
+Route::resource('/lecturer','App\Http\Controllers\LecturerController');
+
+Route::resource('/lecturer','App\Http\Controllers\LecturerController');
+
+Route::resource('/room', 'App\Http\Controllers\RoomController');
