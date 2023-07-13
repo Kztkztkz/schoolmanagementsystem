89bf3338c921d82bc2c0bc4db84b0baea246e0c1
AMY8988 <amymoe2321@gmail.com>
2023-07-13 16:29:05 +0630

    course create finish

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,3 +13,28 @@ $(document).ready(function () {
         $("#hex").val(color);
     });
 });
+
+
+//Course Create
+$("#addCourse").click(function () {
+
+    $("#courseRow").append(`
+        <div class=" mb-3">
+        <a href="" class=" btn course-btn  btn-primary px-1 me-2">
+            Add
+        </a>
+
+        <input type="text" class=" form-control d-inline-block" placeholder="Add new course" >
+        <button class=" btn course-btn btn-secondary px-1 ms-2 course-del">
+            Cancel
+        </button>
+        </div>
+
+    `);
+});
+
+
+// Course Create row delete
+$('.course-row').delegate(".course-del" , "click" , function(){
+    $(this).parent().remove();
+})
